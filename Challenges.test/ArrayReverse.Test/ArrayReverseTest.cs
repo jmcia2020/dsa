37b307e925547200b4b94c2e2cef4ef59932f796
jmcia2020 <jeanmmcmahon@gmail.com>
2021-03-23 17:31:10 -0500

    test works

---
+++ Challenges.test/ArrayReverse.Test/ArrayReverseTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Threading.Tasks;
+
+
+namespace Challenges.test.ArrayReverse.Test
+{
+    public class ArrayReverseTest
+    {
+        [Fact]
+        public void Reverse_returns_empty_array_given_empty_array()
+        {
+            //Arrange
+            //int[] array =
+
+            //Act
+
+            //Assert
+
+        }
+
+            
+            
+    }
+}
---
+++ Challenges.test/ArrayShift.Test/ArrayShiftTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Challenges.ArrayChallenges;
+using System.Threading.Tasks;
+
+namespace Challenges.tests.ArrayChallenges
+{
+    public class ArrayShiftTest
+    {
+        [Fact]
+        public void Array_shift_takes_an_outside_number_and_places_it_in_the_middle_of_a_given_array()
+        {
+            //Arrange
+            int[] array = new int[] {2, 4, 6, 8};
+            int newNum = 5;
+
+            //Act add code
+            int[] results = ArrayShift.InsertShiftArray(array, newNum);
+
+            //Assert
+            int[] expected = new int[] { 2, 4, 5, 6, 8 };
+
+            Assert.Equal(expected, results);
+        }
+
+}
+            
+       
+}
