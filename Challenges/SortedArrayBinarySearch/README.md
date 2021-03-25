76f61beabb33cb519c529c1611bac0265852de71
jmcia2020 <jeanmmcmahon@gmail.com>
2021-03-25 00:03:39 -0500

    binary search pseudocode and whiteboard

---
+++ Challenges/SortedArrayBinarySearch/README.md
@@ -0,0 +1,11 @@
+# Sorted Array Binary Search
+
+Binary Code Search Pseudo Code
+INPUT = Single Sorted Array and a Key
+OUTPUT=  If found = 2/ else = -1
+Start in Center of Array ,
+if int = Key, Output =2
+while int i < Key then Key.Length/2
+while int > Key then Key.Lenth/2
+
+![White Board](../assets/SortedArrayBinarySort.jpg)
---
+++ Challenges/SortedArrayBinarySearch/SortedArrayBinarySearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.SortedArrayBinarySearch
+{
+    class SortedArrayBinarySearch
+    {
+        /*
+        Binary Code Search Pseudo Code
+        INPUT = Single Sorted Array and a Key
+        OUTPUT = If found = 2/ else = -1
+        
+        Start in Center of Array , 
+        if int = Key, Output =2
+        while int i<Key then Key.Length/2
+        while int > Key then Key.Lenth/2
+        Break = -1
+        */
+    }
+}
