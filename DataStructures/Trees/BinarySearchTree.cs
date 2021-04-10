b2c2b8641e4f706a78c8c36d09243e39dc935510
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-09 23:50:54 -0500

    BinaryTree and BinarySearchTree classes added

---
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    public class BinarySearchTree : BinaryTree
+
+    public void Add(int number)
+    {
+
+    }
+
+    public bool Contains(int number)
+    {
+        return fasle;
+    }
+}
+
